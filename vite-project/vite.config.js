b83123f0241fda6ad1aa1cd025d8e926c42bab43
Oscar Grappi <ngrappi@outlook.com>
2023-09-22 17:44:26 -0300

    test gh-pages

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://oscar-grappi.github.io/EntregaFinal-Grappi/dist",
+  base: "/dist",
   plugins: [react()],
 })
